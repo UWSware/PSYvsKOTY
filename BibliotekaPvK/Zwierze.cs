351de427cdd685bb32b3dc302bf0e911e3004d54
pist0ler0 <matii33333@gmail.com>
2025-05-05 09:04:36 +0200

    Zmiana klasy Zwierze

--- BibliotekaPvK/Zwierze.cs
+++ BibliotekaPvK/Zwierze.cs
@@ -8,5 +8,22 @@ namespace BibliotekaPvK
 {
     public abstract class Zwierze
     {
-    }
+		private int _hp;
+
+		public int Hp
+		{
+			get { return _hp; }
+			set { _hp = value; }
+		}
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+
+	}
 }
